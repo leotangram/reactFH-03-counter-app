587065bf431dc2d8647e4d2f45bc25f7a3866b5d
Leonardo Omaña <leotangram@gmail.com>
2020-06-30 10:24:19 -0500

    Nuestro  primer componente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const greating = <h1>Hola mundo</h1>
+import './index.css'
+import FirstApp from './FirstApp'
 
 const divRoot = document.querySelector('#root')
 
-ReactDOM.render(greating, divRoot)
+ReactDOM.render(<FirstApp />, divRoot)
