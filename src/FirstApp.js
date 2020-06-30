f70214678ab74744c8d237b421e97a1d48452f7a
Leonardo Omaña <leotangram@gmail.com>
2020-06-30 11:46:45 -0500

    Default props

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const FirstApp = ({ greeting }) => {
+const FirstApp = ({ greeting, subtitle }) => {
   return (
     <>
       <h1>{greeting}</h1>
       {/* <pre>{JSON.stringify(person, null, 3)}</pre> */}
-      <p>Mi primera aplicación</p>
+      <p>{subtitle}</p>
     </>
   )
 }
 
 FirstApp.propTypes = {
   greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  subtitle: 'Soy un subtitulo'
 }
 
 export default FirstApp
