ffe627b7d8b2d5d355aaeafa0b9137958e019fd5
Leonardo Omaña <leotangram@gmail.com>
2020-06-30 11:54:12 -0500

    Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import FirstApp from './FirstApp'
+import CounterApp from './CounterApp'
 
 const divRoot = document.querySelector('#root')
 
-ReactDOM.render(<FirstApp greeting="Hola, soy Goku" />, divRoot)
+ReactDOM.render(<CounterApp value={123} />, divRoot)
