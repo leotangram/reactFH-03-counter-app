cdd49ea4ffda1f296436262066a1135f17c5a37b
Leonardo Omaña <leotangram@gmail.com>
2020-07-03 15:58:01 -0500

    Pruebas con async await

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -1,32 +1,20 @@
-
-
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+export const getImagen = async () => {
+  try {
+    const apiKey = 'vFZNjEwkQVq9TPbw5gxNjA5zeqxEZmbW'
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    )
+    const { data } = await resp.json()
 
-    try {
+    const { url } = data.images.original
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
+    return url
+  } catch (error) {
+    // manejo del error
+    // console.error(error)
+    return 'No existe'
+  }
 }
-
- getImagen();
-
-
-
