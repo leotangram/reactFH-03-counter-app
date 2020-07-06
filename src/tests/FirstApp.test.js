540baf6ae82ac1ef73528cbb5c2f9c2dccd70140
Leonardo Omaña <leotangram@gmail.com>
2020-07-06 11:22:41 -0500

    Revisar elementos dentro del componente

--- src/tests/FirstApp.test.js
+++ src/tests/FirstApp.test.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+// import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
 import FirstApp from '../FirstApp'
 
 describe('<FirstApp />', () => {
-  test('should show the message "Hola, soy Goku"', () => {
+  // test('should show the message "Hola, soy Goku"', () => {
+  //   const greeting = 'Hola, soy Goku'
+  //   const { getByText } = render(<FirstApp greeting={greeting} />)
+  //   expect(getByText(greeting)).toBeInTheDocument()
+  // })
+
+  test('should show <FirstApp /> correctly', () => {
     const greeting = 'Hola, soy Goku'
-    const { getByText } = render(<FirstApp greeting={greeting} />)
-    expect(getByText(greeting)).toBeInTheDocument()
+    const wrapper = shallow(<FirstApp greeting={greeting} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('should show the subtitle sent by props', () => {
+    const greeting = 'Hola, soy Goku'
+    const subtitle = 'Subtítulo'
+    const wrapper = shallow(
+      <FirstApp greeting={greeting} subtitle={subtitle} />
+    )
+    const textParagraph = wrapper.find('p').text()
+    expect(textParagraph).toBe(subtitle)
   })
 })
