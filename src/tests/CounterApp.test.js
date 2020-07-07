762243e17d7b591d839f9de42d40667340d86bd1
Leonardo Omaña <leotangram@gmail.com>
2020-07-07 15:33:20 -0500

    Simular eventos - click

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -4,15 +4,31 @@ import { shallow } from 'enzyme'
 import CounterApp from '../CounterApp'
 
 describe('CounterApp', () => {
+  let wrapper = shallow(<CounterApp />)
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />)
+  })
+
   test('should show <CounterApp /> correctly', () => {
-    const wrapper = shallow(<CounterApp />)
     expect(wrapper).toMatchSnapshot()
   })
 
   test('should show the counter sent by props', () => {
     const value = 100
     const wrapper = shallow(<CounterApp value={value} />)
-    const number = wrapper.find('h2').text()
+    const number = wrapper.find('h2').text().trim()
     expect(number).toBe('100')
+  })
+
+  test('should increment with +1 button', () => {
+    wrapper.find('button').at(0).simulate('click')
+    const number = wrapper.find('h2').text().trim()
+    expect(number).toBe('11')
+  })
+
+  test('should decrement with -1 button', () => {
+    wrapper.find('button').at(2).simulate('click')
+    const number = wrapper.find('h2').text().trim()
+    expect(number).toBe('9')
   })
 })
